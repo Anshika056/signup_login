471157e1f00e6a2199fbed09c8b9c8cc7aec3301
anshika-056 <anshikamadan2@gmail.com>
2021-11-10 23:45:02 +0530

    background set up

--- reactproject/src/Login/Login.jsx
+++ reactproject/src/Login/Login.jsx
@@ -1,2 +1,18 @@
 import React from "react";
 import "./Login.css"
+function Login(){
+    return(
+        <>
+        <div className="container">
+            <div className="login-box">
+                <h2>Login/Sign in </h2>
+            </div>
+        </div>
+    
+        </>
+
+
+
+    )
+}
+export default Login;
